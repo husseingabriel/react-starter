5e684e30994ea1d8c5c9b7de742959313f5b4028
husseingabriel <husseingabriel@outlook.com>
2023-07-04 21:41:49 -0500

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, {Component} from "react";
 import "./style.css";
+
+function MiComponente(){
+  return <p>Mi Componente</p>
+}
+
+class MiComponenteDeClase extends Component{
+  render(){
+    return <p>Mi Componente de Clase</p>
+  }
+}
 
 export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
+      <MiComponente/>
+      <MiComponenteDeClase/>
     </div>
   );
 }
